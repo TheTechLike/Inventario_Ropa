8f1266352009a44d811da3ee200df1077371b44e
Gabriel Lebron <Juniormontero0.1@gmail.com>
2025-02-16 11:32:57 -0400

    0.16

--- Inventario_Base/Inventario_Base/obj/Debug/net8.0-windows/Inventario_Base.AssemblyInfo.cs
+++ Inventario_Base/Inventario_Base/obj/Debug/net8.0-windows/Inventario_Base.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventario_Base")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e477c0bf5cd661ba17956df293dc36a3fa3bebe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b00b34d306ace79f867f6a18fc1aa01ba1061602")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventario_Base")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventario_Base")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
