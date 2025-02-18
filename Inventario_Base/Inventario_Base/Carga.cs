d64592b1f0aaec80573d80382d7e00e58781ab70
Gabriel Lebron <Juniormontero0.1@gmail.com>
2025-02-18 19:40:23 -0400

    0.24.5

--- Inventario_Base/Inventario_Base/Carga.cs
+++ Inventario_Base/Inventario_Base/Carga.cs
@@ -89,6 +89,8 @@ namespace Inventario_Base
                 else
                 {
                    progressBar1.Value = 90;
+
+                    timer1.Start();
                 }
 
                 return false;
